338d678dc85ffe938c3055add89609845e216973
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-06 03:28:00 +0530

    Integrate AddTodo and Todos into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className='bg-gray-400 w-full h-screen'>
-      
+    <div className='bg-gray-500 h-screen rounded-2xl'>
+      <h1 className='font-bold text-3xl p-5'>Todo List</h1>
+      <AddTodo/>
+      <Todos/>
     </div>
   )
 }

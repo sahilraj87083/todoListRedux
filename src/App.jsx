511da393758d4169c3c75d97867582dd552448a0
sahilraj87083 <sahilraj87093@gmail.com>
2025-12-06 02:27:35 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-gray-400 w-full h-screen'>
+      
+    </div>
+  )
+}
+
+export default App
